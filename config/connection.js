da89f3cf66aba68d3ebe363b71e01e4f5e25cd39
Stepan <stepos36@gmail.com>
2019-02-26 14:57:07 -0500

    connection.js

---
+++ config/connection.js
@@ -0,0 +1,31 @@
+var mysql = require("mysql")
+    PORT = process.env.PORT || 8080;
+    connection = mysql.createConnection({
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: "root",
+      database: "burgers_db"
+    });
+
+handleDisconnect()
+console.log("connected as id " + connection.threadId);
+
+module.exports = connection
+
+function handleDisconnect() {
+  connection.connect(function(err) {
+    if(err) {
+      console.error("error connecting: " + err);
+      setTimeout(handleDisconnect, 2000);
+      }
+  });
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers(burger_name)
+VALUES ('Spicy Salmon Deluxe'),('Homemade Angus Beef Burger'),('Chipotle Chicken Avocado burger')
