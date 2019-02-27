1580429f3248a45efe55bb56d39652bafca9ee6b
Stepan <stepos36@gmail.com>
2019-02-26 22:25:16 -0500

    JAWSDB credentials added

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 var mysql = require("mysql")
     PORT = process.env.PORT || 8080;
-    connection = mysql.createConnection({
-      host: "localhost",
-      port: 3306,
-      user: "root",
-      password: "root",
-      database: "burgers_db"
-    });
-
+    connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burgers_db"
+  });
+}
 handleDisconnect()
 console.log("connected as id " + connection.threadId);
 
